9bfe577cdb0fe0cfe16ec507559860923af3615e
mango0002 <104410847+agkhantsoe@users.noreply.github.com>
2022-09-06 21:45:45 +0630

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 export default {
   root: path.resolve(__dirname, "src"),
+  base: '/ZLand-2months-project/',
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
